ef5e716ab5afbcf17de31a338af0481c3f1c6c55
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-09-04 19:22:08 +0200

    prompt and boundaries

--- app.js
+++ app.js
@@ -47,29 +47,46 @@ function turnRight(rover){
   return rover.direction;
 }
 
+function boundariesCheck(rover) {
+if ( rover.direction === "N" && rover.travelLog[1] < 9 ) {
+      return true;
+} else if ( rover.direction === "S" && rover.travelLog[1] > 0 ) {
+    return true;
+} else if ( rover.direction === "E" && rover.travelLog[0] < 9 ) {
+        return true;
+} else if ( rover.direction === "W" && rover.travelLog[0] > 0 ) {
+        return true;
+} else
+        return false;
+}
+
 function moveForward(rover){
   console.log("moveForward was called");
   var moving = rover.direction;
-  switch (moving) {
-    case "N":
-      rover.travelLog[1] -= 1;
-      break;
-    case "S":
+  if (boundariesCheck(rover)) {
+    switch (moving) {
+      case "N":
       rover.travelLog[1] += 1;
-      break;
-    case "E":
-      rover.travelLog[0] += 1;
-      break;
-    case "W":
-      rover.travelLog[0] -= 1;
-      break;
-  }
+        break;
+      case "S":
+        rover.travelLog[1] -= 1;
+        break;
+      case "E":
+        rover.travelLog[0] += 1;
+        break;
+      case "W":
+        rover.travelLog[0] -= 1;
+        break;
+    }
   return [rover.travelLog[0], rover.travelLog[1]];
+  } else {
+  console.log("the rover is out of boundaries!!");
+  }
 }
 
 function keyShortcuts() {
   console.log("keyShortcuts was used");
-  var str = "lrflfrff";
+  var str = prompt("Direct with (f)orward, (l)eft and (r)ight");
   for (var i = 0; i < str.length; i++) {
     switch (str[i]) {
       case "l":
@@ -86,5 +103,3 @@ function keyShortcuts() {
   }
   return [rover.direction, rover.travelLog[0], rover.travelLog[1]];
 }
-
-/*addig comments */
